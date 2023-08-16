855b3ab518c0e9fa6ae98d09481c8ddb8ac223e7
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-16 13:01:31 -0300

    adding new pages

---
+++ pages/more.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const More = () => (
+    <Layout>
+        <h1>More</h1>
+    </Layout>
+)
+
+export default More;
---
+++ pages/services.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const Services = () => (
+    <Layout>
+        <h1>Services</h1>
+    </Layout>
+)
+
+export default Services;
