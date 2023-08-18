03322d7b4e44256dd9fe6975b523736163e187fe
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-18 16:38:00 -0300

    creanted skills and experiences

---
+++ data/experiences.js
@@ -0,0 +1,8 @@
+export const experiences = [
+    {
+        title: "Auxiliar E-Commerce in Jumbo Online-Cencosud S.A.",
+        description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno",
+        from: 2021,
+        to: 2022
+    }
+]
---
+++ data/skills.js
@@ -0,0 +1,12 @@
+export const skills = [
+    {
+        skill: "JavaScript",
+        porcentage: 100
+    },{
+        skill: "Node.js",
+        porcentage: 100
+    },{
+        skill: "English",
+        porcentage: 60
+    },
+]
