d21725c31a0ed92167e549793f4d388a4cd0b912
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-25 19:16:22 -0300

    adding Footer in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
+import Footer from "./Footer";
 import NavBar from "./NavBar";
 
 const Layout = ({ children }) => (
     <>
         <NavBar/>
-        <main className="container py-4">
+        <main className="container py-4 bg-dark">
             {children}
         </main>
+        <Footer/>
     </>
 )
 
