d78ab182bac82f9b2cf499681e8835f4f15701bc
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-26 11:59:30 -0300

    changes in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,10 @@
 const Footer = () => (
-    <div></div>
+    <div className="bg-dark text-light text-center">
+        <div className="container p-4">
+            <h4>@copyright; Roberto Rocha Portfolio</h4>
+            <p>2023 - {new Date().getFullYear()}</p>
+            <p>All Rights Reserverd</p>
+        </div>
+    </div>
 )
  export default Footer;
