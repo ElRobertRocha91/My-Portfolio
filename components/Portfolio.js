65d38042b1a4c41dcf7902a885fece198a2bf161
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-26 12:43:17 -0300

    adding components/Portfolio

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,5 +1,28 @@
+import Link from "next/link";
+import { Projects } from "../data/projects";
+
 const Portfolio = () => (
-    <div></div>
+    <div className="container text-center overflow-hidden bg-light">
+        <h1 className="text-center text-dark">Portfolio</h1>
+        <div className="row gx-5">
+            {
+                Projects.map(({ name, description, image }, index) => (
+                    <div className="col" key={index}>
+                        <div className="card p-3 mb-4 bg-dark h-90 d-flex flex-column justify-content-between">
+                            <img src={`/${image}`} alt="img" className="card-img-top img-fluid" />
+                        </div>
+                        <div className="card-body text-light">
+                            <h3>{name}</h3>
+                            <p>{description}</p>
+                            <Link href="/more">
+                                <button className="btn btn-primary">Know more</button>
+                            </Link>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
 )
 
 export default Portfolio;
