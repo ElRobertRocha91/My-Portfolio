82c9af6014990c6a372e9d699bbbe89d3855329b
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-09-08 20:56:20 -0300

    adding component loading

---
+++ components/Loading.js
@@ -0,0 +1,10 @@
+const Loading = () => (
+    <div className="container">
+        <div className="custom-container">
+            <div className="custom-loader"></div>
+            <p className="custom-message">Loading...</p>
+        </div>
+    </div>
+)
+
+export default Loading;
