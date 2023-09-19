5f94e70e775fcaec783eb8ea5e37529def34f13b
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-09-18 23:33:56 -0300

    new component contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,22 @@
 const Contact = () => (
-    <div>Contact</div>
+    <section className="row text-light text-center bg-black bg-gradient">
+        <h2>
+            Contact Me
+        </h2>
+        <div>
+            <form>
+                <div>
+                    <input type="text" className="w-25" required placeholder="Enter your name"/>
+                    <input type="text" className="w-25" required placeholder="Enter your email"/>
+                </div>
+                <textarea className="w-50" required placeholder="Enter your message"></textarea>
+                <p>Message</p>
+                <button type="submit" className="btn btn-outline-success">
+                    Send Message
+                </button>
+            </form>
+        </div>
+    </section>
 )
 
 export default Contact;
