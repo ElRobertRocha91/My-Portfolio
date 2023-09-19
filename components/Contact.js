95e3ad87c2362e76a98459b4154bf9976b3f7ef2
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-09-19 12:42:26 -0300

    adding style in Contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,20 +1,28 @@
 const Contact = () => (
-    <section className="row text-light text-center bg-black bg-gradient">
-        <h2>
-            Contact Me
-        </h2>
-        <div>
-            <form>
-                <div>
-                    <input type="text" className="w-25" required placeholder="Enter your name"/>
-                    <input type="text" className="w-25" required placeholder="Enter your email"/>
-                </div>
-                <textarea className="w-50" required placeholder="Enter your message"></textarea>
-                <p>Message</p>
-                <button type="submit" className="btn btn-outline-success">
-                    Send Message
-                </button>
-            </form>
+    <section className="row text-light text-center bg-black bg-gradient d-flex justify-content-center mt-4">
+        <div className="card w-50 mb-3 bg-dark border-primary">
+            <h2 className="custom-title">
+                Contact Me
+            </h2>
+            <div className="mb-3 px-3 py-3">
+                <form>
+                    <div>
+                        <div className="mb-3">
+                            <input type="text" className="custom-input form-control" required placeholder="Enter your name" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="text" className="custom-input form-control" required placeholder="Enter your email" />
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <textarea className="custom-input form-control" required placeholder="Enter your message"></textarea>
+                    </div>
+                    <p>Message</p>
+                    <button type="submit" className="btn btn-outline-success custom-btn">
+                        Send Message
+                    </button>
+                </form>
+            </div>
         </div>
     </section>
 )
