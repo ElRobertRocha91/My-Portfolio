8d6337308230718dd50cbb7c35af14210b0f9195
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-09-26 22:04:11 -0300

    adding etiqueta

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,5 +1,23 @@
 const AboutMe = () => (
-    <div>AboutMe</div>
+    <div>
+        <div>
+            <h4>About Me</h4>
+            <div>
+                <p>
+                    Hello! my name is Roberto Rocha, I am a Full Stack Web Developer Jr.
+                    interested and passionate about technology, I starded my training ot the 
+                    National Technological University as a University Technician in Programing.
+                    With the purpose of becoming a Software Developer.
+                    Currently acquiring new knowledge and applying them in proyects that may be of 
+                    your interest, I invite you to kmoew them in the following section.
+                </p>
+            </div>
+            <div>
+                <h4>Hard Skills</h4>
+                <div>Array of object</div>
+            </div>
+        </div>
+    </div>
 )
 
 export default AboutMe;
