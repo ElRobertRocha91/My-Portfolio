040a1dff03f328282a91f04a440e21d4384e0952
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-10-06 14:32:47 -0300

    styles skills

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -7,27 +7,29 @@ const AboutMe = () => (
             <div className="container-md text-start fs-4 p-5 px-10">
                 <p>
                     Hello! my name is Roberto Rocha, I am a Full Stack Web Developer Jr.
-                    interested and passionate about technology, I starded my training ot the 
+                    interested and passionate about technology, I starded my training ot the
                     National Technological University as a University Technician in Programing.
                     With the purpose of becoming a Software Developer.
-                    Currently acquiring new knowledge and applying them in proyects that may be of 
+                    Currently acquiring new knowledge and applying them in proyects that may be of
                     your interest, I invite you to kmoew them in the following section.
                 </p>
             </div>
             <div className="container">
-                <h4 className="custom-title">Hard Skills</h4>
+                <h2 className="custom-title">Hard Skills</h2>
                 <div className="d-flex flex-wrap justify-content-between">
                     <ul className="list-unstyled row">
                         {
-                            skills.map(({skill, image}, index) => (
-                                <li key={index} className="col-md-4 mb-3 text-center">
-                                    <div>
-                                        <img src={`/${image}`} alt="image" 
-                                        className="img-fluid" 
-                                        style={{maxWidth: "100px", maxHeight: "100px"}}
-                                        />
+                            skills.map(({ skill, image }, index) => (
+                                <li key={index} className="col-md-4 mb-3">
+                                    <div className="card bg-dark text-center border-primary">
+                                        <div className="mt-2">
+                                            <img src={`/${image}`} alt="image"
+                                                className=" card-img-top img-fluid"
+                                                style={{ maxWidth: "100px", maxHeight: "100px" }}
+                                            />
+                                            <h4 className="card-title text-light mt-2">{skill}</h4>
+                                        </div>
                                     </div>
-                                    <h4>{skill}</h4>
                                 </li>
                             ))
                         }
