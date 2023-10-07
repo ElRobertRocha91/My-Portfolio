ae54bff150f770b07265c4659f3877ffc5d395c6
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-10-07 00:00:22 -0300

    changes in portfolio

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -9,14 +9,16 @@ const Portfolio = () => (
                 Projects.map(({ name, description, image }, index) => (
                     <div className="col" key={index}>
                         <div className="card p-3 mb-4 bg-dark h-90 d-flex flex-column justify-content-between">
-                            <img src={`/${image}`} alt="img" className="card-img-top img-fluid" />
-                        </div>
-                        <div className="card-body text-light">
-                            <h3>{name}</h3>
-                            <p>{description}</p>
-                            <Link href="/more">
-                                <button className="btn btn-primary">Know more</button>
-                            </Link>
+                            <div className="overflow">
+                                <img src={`/${image}`} alt="img" className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body text-light">
+                                <h3>{name}</h3>
+                                <p>{description}</p>
+                                <Link href="/more">
+                                    <button className="btn btn-primary">Know more</button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 ))
