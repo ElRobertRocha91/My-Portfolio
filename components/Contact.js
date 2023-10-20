179041ec81eac1b3807d17b93b2425fa94b3359a
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-10-20 18:27:45 -0300

    contact message

--- components/Contact.js
+++ components/Contact.js
@@ -1,30 +1,91 @@
-const Contact = () => (
-    <section className="row text-light text-center bg-black d-flex justify-content-center mt-4">
-        <div className="card w-50 mb-3 mt-3 bg-dark border-primary">
-            <h2 className="custom-title">
-                Contact Me
-            </h2>
-            <div className="mb-3 px-3 py-3">
-                <form>
-                    <div>
-                        <div className="mb-3">
-                            <input type="text" className="custom-input form-control" required placeholder="Enter your name" />
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    const form = useRef(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm("service_zgt1kbl", "templane_k1v8um3", form.current, "yF2QjO9nUK9rkeeH0")
+            .then(
+                (result) => {
+                    alert("Message sent successfully");
+                    setFormData({
+                        name: "",
+                        email: "",
+                        message: ""
+                    });
+                },
+                (error) => {
+                    alert("Message failed to send. Please try again later.");
+                }
+            )
+    }
+
+    return (
+        <section className="row text-light text-center bg-black d-flex justify-content-center mt-4">
+            <div className="card w-50 mb-3 mt-3 bg-dark border-primary">
+                <h2 className="custom-title">
+                    Contact Me
+                </h2>
+                <div className="mb-3 px-3 py-3">
+                    <form onSubmit={handleSubmit} ref={form}>
+                        <div>
+                            <div className="mb-3">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    className="custom-input form-control"
+                                    required placeholder="Enter your name"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className="custom-input form-control"
+                                    required placeholder="Enter your email"
+                                />
+                            </div>
                         </div>
                         <div className="mb-3">
-                            <input type="text" className="custom-input form-control" required placeholder="Enter your email" />
+                            <textarea
+                                name="message"
+                                value={formData.message}
+                                onchange={handleChange}
+                                className="custom-input form-control"
+                                required placeholder="Enter your message"
+                            ></textarea>
                         </div>
-                    </div>
-                    <div className="mb-3">
-                        <textarea className="custom-input form-control" required placeholder="Enter your message"></textarea>
-                    </div>
-                    <p>Message</p>
-                    <button type="submit" className="btn btn-outline-success custom-btn">
-                        Send Message
-                    </button>
-                </form>
+                        <p>Message</p>
+                        <button type="submit" className="btn btn-outline-success custom-btn">
+                            Send Message
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </section>
-)
+        </section>
+    )
+}
 
 export default Contact;
