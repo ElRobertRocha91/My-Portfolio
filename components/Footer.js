8c9d5a214c82457d7079f9b7b2972e728a1c2394
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-11-13 21:38:28 -0300

    adding changes to correct react component footer hydration

--- components/Footer.js
+++ components/Footer.js
@@ -1,30 +1,40 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
-const Footer = () => (
-    <div className="custom-bg-black text-light text-center">
-        <div className="container">
-            <h3>My Portfolio</h3>
-            <div className="custom-footer-links d-flex justify-content-between">
-                <ul className="list-inline flex-fill">
-                    <li className="list-inline-item">
-                        <Link href="/about" className="link text-light">About</Link>
-                    </li>
-                    <li className="list-inline-item">
-                        <Link href="/blog" className="link text-light">Blog</Link>
-                    </li>
-                    <li className="list-inline-item">
-                        <Link href="/services" className="link text-light">Services</Link>
-                    </li>
-                    <li className="list-inline-item">
-                        <Link href="/more" className="link text-light">More</Link>
-                    </li>
-                </ul>
+const Footer = () => {
+    const [date, setDate] = useState(null);
+
+    useEffect(() => {
+        setDate(new Date().getFullYear());
+    }, []);
+
+    return (
+        <div className="custom-bg-black text-light text-center">
+            <div className="container">
+                <h3>My Portfolio</h3>
+                <div className="custom-footer-links d-flex justify-content-between">
+                    <ul className="list-inline flex-fill">
+                        <li className="list-inline-item">
+                            <Link href="/about" className="link text-light">About</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link href="/blog" className="link text-light">Blog</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link href="/services" className="link text-light">Services</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link href="/more" className="link text-light">More</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="container p-4">
+                <h6>@copyright, All Rights Reserverd Roberto Rocha</h6>
+                <p>2023 - {date}</p>
             </div>
         </div>
-        <div className="container p-4">
-            <h6>@copyright, All Rights Reserverd Roberto Rocha</h6>
-            <p>2023 - {new Date().getFullYear()}</p>
-        </div>
-    </div>
-)
- export default Footer;
+    )
+}
+
+export default Footer;
