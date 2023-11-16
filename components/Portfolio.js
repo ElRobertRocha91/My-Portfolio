44ea4964580414c1862b2a9df9774ed4833f49e2
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-11-16 18:50:23 -0300

    new height

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -10,9 +10,9 @@ const Portfolio = () => (
                     <div className="col" key={index}>
                         <div className="card p-3 mb-4 bg-dark h-90 d-flex flex-column justify-content-between border-primary">
                             <div className="overflow">
-                                <img src={`/${image}`} alt="img" className="card-img-top img-fluid" style={{ height: "100px", width: "auto" }} />
+                                <img src={`/${image}`} alt="img" className="card-img-top img-fluid" style={{ height: "200px", width: "auto" }} />
                             </div>
-                            <div className="card-body text-light" style={{ maxHeight: "100px", overflow: "hidden" }}>
+                            <div className="card-body text-light">
                                 <h3>{name}</h3>
                                 <p>{description}</p>
                                 <div className="d-grid gap-2 d-md-block">
