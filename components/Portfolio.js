60735496589dea710fbf49413c4472490c9c5bfc
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-11-16 17:56:04 -0300

    changes in styles

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -8,11 +8,11 @@ const Portfolio = () => (
             {
                 Projects.map(({ name, description, image, repository, url }, index) => (
                     <div className="col" key={index}>
-                        <div className="card p-3 mb-4 bg-dark h-90 d-flex flex-column justify-content-between border-primary">
+                        <div className="card p-3 mb-4 bg-dark h-100 d-flex flex-column justify-content-between border-primary">
                             <div className="overflow">
                                 <img src={`/${image}`} alt="img" className="card-img-top img-fluid" />
                             </div>
-                            <div className="card-body text-light">
+                            <div className="card-body text-light" style={{ maxHeight: "150px", overflow: "hidden" }}>
                                 <h3>{name}</h3>
                                 <p>{description}</p>
                                 <div className="d-grid gap-2 d-md-block">
