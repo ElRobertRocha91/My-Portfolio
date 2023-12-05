f35881ac7d7588fbb480252e56bd816bd83c0bda
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-12-04 22:11:36 -0300

    Page development

--- pages/more.js
+++ pages/more.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const More = () => (
     <Layout>
-        <h1 className="mt-4 text-light text-center">Oops! Sorry this page is under development</h1>
+        <h1 className="text-light text-center">Oops! Sorry this page is under development</h1>
     </Layout>
 )
 
