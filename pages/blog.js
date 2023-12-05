4d9ca3725e57de6fac682078451758182a9a8d7c
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-12-04 22:08:34 -0300

    Page in development

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,8 @@
 import Layout from "../components/Layout";
 
 const Blog = () => (
-    <Layout>
-        <div className="card text-center">
-            <h1 className="mt-4 text-light">Oops! Sorry this page is under development</h1>
-        </div>
+    <Layout>   
+        <h1 className="mt-4 text-light text-center">Oops! Sorry this page is under development</h1> 
     </Layout>
 )
 
--- pages/more.js
+++ pages/more.js
@@ -2,9 +2,7 @@ import Layout from "../components/Layout";
 
 const More = () => (
     <Layout>
-        <div className="card text-center">
-            <h1 className="mt-4 text-light">Oops! Sorry this page is under development</h1>
-        </div>
+        <h1 className="mt-4 text-light text-center">Oops! Sorry this page is under development</h1>
     </Layout>
 )
 
